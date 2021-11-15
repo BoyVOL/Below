f856fe52ac2b2e0d3140b0f96687a27df324058d
BoyVOL <vladislav_limonov@mail.ru>
2021-11-15 12:42:59 +0900

    1

--- Scripts/GroundMap.cs
+++ Scripts/GroundMap.cs
@@ -1,41 +1,74 @@
 using Godot;
 using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace GroundMap{
 
-    public struct MapPoint{
-        int TileID;
+    public struct MapCell{
+        public int TileID;
     }
 
     /// <summary>
-    /// Класс, отвечающий за загрузку карты в физическом варианте
+    /// Класс, отвечающий за загрузку части карты в игре
     /// </summary>
-    public class localMap{
+    public class Sector{
+        public TileMap Viewport = new TileMap();
+
+        public MapCell[,] Cells;
+
+        public Sector(int Size){
+            Cells = new MapCell[Size,Size];
+        }
+
+        public void Visualise(){
+            for (int i = 0; i < Cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < Cells.GetLength(1); j++)
+                {
+                    Viewport.SetCell(i,j,Cells[i,j].TileID);
+                }
+            }
+        }
+    }
+
+    public class MapSerialiser{
         
+        BinaryFormatter Formatter = new BinaryFormatter();
+
+        MemoryStream stream;
+        public Byte[] Serialise(MapCell[,] cells){
+            stream = new MemoryStream();
+            Formatter.Serialize(stream,cells);
+            return stream.ToArray();
+        }
+
+        public MapCell[,] Deserialise(Byte[] Data){
+            stream = new MemoryStream(Data);
+            return (MapCell[,])Formatter.Deserialize(stream);
+        }
     }
 
     /// <summary>
-    /// Класс для создания глобальной карты
+    /// Класс для создания и сохранения глобальной карты
     /// </summary>
     public class GlobalMap{
 
+        /// <summary>
+        /// Поле, содержащее, куда надо подгружать карту
+        /// </summary>
+        public Node2D Parent = null;
+
+        /// <summary>
+        /// Поле, содержащее глобальный тайлсет для карты
+        /// </summary>
+        public TileSet Set = null;
     }
     
     /// <summary>
     /// Класс для генерации глобальной карты
     /// </summary>
     public class MapGenerator{
-        
-    }
-
-    /// <summary>
-    /// Класс, отвечающий за отображение карты на экране
-    /// </summary>
-    public class MapVisual{
-        public localMap Map;
-
-        public TileMap Current = new TileMap();
-
         
     }
 }
--- TestScene.cs
+++ TestScene.cs
@@ -5,7 +5,7 @@ using GroundMap;
 public class TestScene : Node2D
 {
     Random RND = new Random();
-    MapVisual Map = new MapVisual();
+    Sector Map = new Sector(100);
 
     TileSet MapTileSet = ResourceLoader.Load<TileSet>("res://TileSets/TestTileset.tres");
     // Declare member variables here. Examples:
@@ -15,19 +15,13 @@ public class TestScene : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        Map.Current.TileSet = MapTileSet;
-        Map.Current.CellSize = new Vector2(10,10);
-        this.AddChild(Map.Current);
+        Map.Viewport.TileSet = MapTileSet;
+        Map.Viewport.CellSize = new Vector2(10,10);
+        this.AddChild(Map.Viewport);
     }
 
     public override void _Process(float delta)
     {
-        for (int i = 0; i < 100; i++)
-        {
-            for (int j = 0; j < 100; j++)
-            {
-                Map.Current.SetCellv(new Vector2(i,j),RND.Next(-1,4));
-            }
-        }
+        Map.Visualise();
     }
 }
