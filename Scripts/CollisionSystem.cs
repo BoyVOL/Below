286c5cd836c5d179cdba056c48ce5fa71c147fe3
BoyVOL <vladislav_limonov@mail.ru>
2021-12-02 18:14:02 +0900

    1

--- Scripts/CollisionSystem.cs
+++ Scripts/CollisionSystem.cs
@@ -6,10 +6,47 @@ namespace MapSystem
     /// Абстрактный класс для определения сетки коллизий
     /// </summary>
     public abstract class CollGrid{
-        Transform2D Transform;
 
-        Transform2D Shift;
+        public Vector2 Position;
 
-        Vector2 Size;
+        public float Rotation;
+
+        /// <summary>
+        /// Массив размеров сетки коллизий
+        /// </summary>
+        public int[] size = new int[2];
+
+        float CellSize;
+
+        /// <summary>
+        /// Метод для проверки одного столкновения по указанным индексам
+        /// </summary>
+        /// <param name="Xid"></param>
+        /// <param name="Yid"></param>
+        public abstract void CalcCollision(int Xid, int Yid);
+
+        /// <summary>
+        /// Метод, возвращающий глобальные координаты по указанным индексам
+        /// </summary>
+        /// <param name="Xid"></param>
+        /// <param name="Yid"></param>
+        /// <returns></returns>
+        public Vector2 GetCoords(int Xid, int Yid){
+            Vector2 Result = new Vector2(Position.x+Xid*CellSize, Position.y+Yid*CellSize).Rotated(Rotation);
+            return Result;
+        }
+
+        /// <summary>
+        /// Метод для проверки столкновений по всей сетке
+        /// </summary>
+        public void CalcAllCollisions(){
+            for (int i = 0; i < size[0]; i++)
+            {
+                for (int j = 0; j < size[1]; j++)
+                {
+                    CalcCollision(i,j);
+                }
+            }
+        }
     }
 }
