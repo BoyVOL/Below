2e23668be1760fd7eeb924720f3e6a57bd5203c1
BoyVOL <vladislav_limonov@mail.ru>
2022-11-03 17:14:47 +0900

    1

--- addons/PhysPolygon/PhysPolygon.cs
+++ addons/PhysPolygon/PhysPolygon.cs
@@ -4,13 +4,27 @@ using System;
 [Tool]
 public class PhysPolygon : EditorPlugin
 {
+    public string LibDir = "res://addons/PhysPolygon/";
+
+    public void AddCustomType(string Type, string parent){
+        AddCustomType(Type,parent,GD.Load<Script>(LibDir+Type+"/"+Type+".cs"),
+        GD.Load<Texture>(LibDir+"/"+Type+"/icon.png"));
+    }
+
+    public void AddAutoloadSingleton(string Name){
+        AddAutoloadSingleton(Name,LibDir+"/AutoloadScenes/"+Name+".tscn");
+    }
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public override void _EnterTree()
     {
+        
+    }
+
+    public override void _ExitTree(){
         
     }
 
